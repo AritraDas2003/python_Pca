0572b265fe4426dfad368eec9a4313075f07ec17
Aritra Das <aritragamer007@gmail.com>
2026-02-21 21:53:54 +0530

    Add grading program to calculate total and aggregate marks

---
+++ 2/Grading.py
@@ -0,0 +1,27 @@
+marks =[]
+
+for i in range(1,5):
+  mark =float(input(f"Enter the marks of subject {i}: "))
+  if 0<= mark <=100:
+    marks.append(mark)
+  else:
+    print("Invalid mark, please enter a number between 0 and 100")
+    exit()
+
+#sum of marks
+total=0
+for mark in marks:
+  total = total + mark
+
+aggregate = total/4
+
+print(f"total marks:{total}")
+print(f"Aggregate marks:{aggregate}")
+
+if aggregate >=60:
+  print("1st division")
+elif aggregate >=45:
+  print("2nd division")
+elif aggregate >=30:
+  print("3rd division")
+else: print ("Fail")
